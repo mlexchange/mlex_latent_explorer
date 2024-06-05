a39cc3b9be9dda4a2c1b844456a2a39e3786802d
taxe10 <tanchavez@lbl.gov>
2024-06-05 14:23:37 -0700

    Add conftest and update requirements

---
+++ src/test/conftest.py
@@ -0,0 +1,26 @@
+import pytest
+from tiled.catalog import from_uri
+from tiled.client import Context
+from tiled.server.app import build_app
+
+
+@pytest.fixture
+def catalog(tmpdir):
+    adapter = from_uri(
+        f"sqlite+aiosqlite:///{tmpdir}/catalog.db",
+        writable_storage=str(tmpdir),
+        init_if_not_exists=True,
+    )
+    yield adapter
+
+
+@pytest.fixture
+def app(catalog):
+    app = build_app(catalog)
+    yield app
+
+
+@pytest.fixture
+def context(app):
+    with Context.from_app(app) as context:
+        yield context
