49b4f76ddc5664f5e18a8bf1bd5aa15a1bd235ba
Dylan McReynolds <dmcreynolds@lbl.gov>
2025-05-27 16:16:55 -0700

    Move LSE Operator from arroyosas

---
+++ src/arroyo/publisher.py
@@ -0,0 +1,93 @@
+import asyncio
+import json
+import logging
+from typing import Union
+
+# import msgpack
+# import numpy as np
+import websockets
+from arroyopy.publisher import Publisher
+
+from .schemas import LatentSpaceEvent, SASStart, SASStop
+
+
+logger = logging.getLogger(__name__)
+
+
+class LSEWSResultPublisher(Publisher):
+    """
+    A publisher class for sending dimensionality reduction information
+
+    """
+
+    websocket_server = None
+    connected_clients = set()
+    current_start_message = None
+
+    def __init__(self, host: str = "localhost", port: int = 8001):
+        super().__init__()
+        self.host = host
+        self.port = port
+
+    async def start(
+        self,
+    ):
+        # Use partial to bind `self` while matching the expected handler signature
+        server = await websockets.serve(
+            self.websocket_handler,
+            self.host,
+            self.port,
+        )
+        logger.info(f"Websocket server started at ws://{self.host}:{self.port}")
+        await server.wait_closed()
+
+    async def publish(self, message: LatentSpaceEvent) -> None:
+        if self.connected_clients:  # Only send if there are clients connected
+            asyncio.gather(
+                *(self.publish_ws(client, message) for client in self.connected_clients)
+            )
+
+    async def publish_ws(
+        self,
+        client: websockets.ServerConnection,
+        message: Union[LatentSpaceEvent | SASStart | SASStop],
+    ) -> None:
+        if isinstance(message, SASStop):
+            # logger.info(f"WS Sending Stop {message}")
+            # self.current_start_message = None
+            # await client.send(json.dumps(message.model_dump()))
+            return
+
+        if isinstance(message, SASStart):
+            # self.current_start_message = message
+            # logger.info(f"WS Sending Start {message}")
+            # await client.send(json.dumps(message.model_dump()))
+            return
+
+        if isinstance(message, LatentSpaceEvent):
+            # send image data separately to client memory issues
+            message = {
+                "tiled_uri": message.tiled_url,
+                "index": message.index,
+                "feature_vector": message.feature_vector,
+            }
+            logger.debug(f"WS Sending LatentSpaceEvent {message['feature_vector']}")
+            await client.send(json.dumps(message))
+
+    async def websocket_handler(self, websocket):
+        logger.info(f"New connection from {websocket.remote_address}")
+        # if websocket.request.path != "/viz":
+        #     logger.info(f"Invalid path: {websocket.request.path}, we only support /viz")
+        #     return
+        self.connected_clients.add(websocket)
+        try:
+            # Keep the connection open and do nothing until the client disconnects
+            await websocket.wait_closed()
+        finally:
+            # Remove the client when it disconnects
+            self.connected_clients.remove(websocket)
+            logger.info("Client disconnected")
+
+    @classmethod
+    def from_settings(cls, settings: dict) -> "LSEWSResultPublisher":
+        return cls(settings.host, settings.port)
---
+++ src/arroyo/schemas.py
@@ -0,0 +1,39 @@
+import numpy as np
+from arroyopy.schemas import Event
+from pydantic import BaseModel, field_serializer, field_validator
+
+
+
+class SerializableNumpyArrayModel(BaseModel):
+    """
+    Custom Pydantic model for serializing NumPy arrays.
+    """
+
+    array: np.ndarray
+
+    @field_serializer("array")
+    def serialize_array(self, value: np.ndarray):
+        """Convert NumPy array to a dictionary with bytes and dtype"""
+        return {
+            "data": value.tobytes(),
+            "dtype": str(value.dtype.name),
+            "shape": value.shape,
+        }
+
+    @field_validator("array", mode="before")
+    @classmethod
+    def deserialize_array(cls, value):
+        """Convert bytes back to NumPy array"""
+        if isinstance(value, dict) and "data" in value:
+            return np.frombuffer(value["data"], dtype=np.dtype(value["dtype"])).reshape(
+                value["shape"]
+            )
+        return value
+
+    class Config:
+        arbitrary_types_allowed = True
+
+class LatentSpaceEvent(Event):
+    tiled_url: str
+    feature_vector: list[float]
+    index: int
